518ed0ce8f0846355b0725212dae7c122be712b6
Mark-david <ojukwudavid59@outlook.com>
2023-04-07 14:10:39 +0100

    Updates

---
+++ js/style-switcher.js
@@ -0,0 +1,21 @@
+const styleSwitcherToggler = document.querySelector('.style-switcher-toggler');
+styleSwitcherToggler.addEventListener('click', () => {
+  document.querySelector('.style-switcher').classList.toggle('open');
+});
+
+window.addEventListener('scroll', () => {
+  if (document.querySelector('.style-switcher').classList.contains('open')) {
+    document.querySelector('.style-switcher').classList.remove('open');
+  }
+});
+
+const alternateStyles = document.querySelectorAll('.alternate-style');
+const setActiveStyle = color => {
+  alternateStyles.forEach(style => {
+    if (color === style.getAttribute('title')) {
+      style.removeAttribute('disabled');
+    } else {
+      style.setAttribute('disabled', 'true');
+    }
+  });
+};
